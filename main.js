606e0a9638146ca3a9b4d5026af1953774a89293
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-01-20 09:47:32 -0300

    javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+const displayPrev = document.querySelector('.display-previous');
+const displayResult = document.querySelector('.display-result');
+const numbers = document.querySelectorAll('[id*=key]');
+const operaters = document.querySelectorAll('[id*=operater]');
+const equal = document.querySelector('.equal');
