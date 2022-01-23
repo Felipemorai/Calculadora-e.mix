7836bd848208d401604e4ac91ed5e4e2cd47f71f
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-01-23 09:43:13 -0300

    dot

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ const displayResult = document.querySelector('.display-result');
 const numbers = document.querySelectorAll('[id*=key]');
 const operaters = document.querySelectorAll('[id*=operater]');
 const equal = document.querySelector('.equal');
+const allclear = document.querySelector('.all-clear');
+const deletelast = document.querySelector('.delete');
+
+let disPrev = '';
+let disRes = null;
+let lastOperation = '';
+let dot = false;
+
+numbers.forEach( number => {
+    number.addEventListener('click', (e) => {
+        if( e.target.innerText === '.' && !dot ) {
+            dot = true;
+        }else
+        if(e.target.innerText === '.' && dot) {
+            return;
+        }
+    });
+});
