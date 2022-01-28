3fae0205408d98e343342d1510a894860398e0e0
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-01-28 09:43:57 -0300

    clear

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const resultDisplay = document.querySelector('.display-result');
 const numbers = document.querySelectorAll('[id*=key]');
 const operaters = document.querySelectorAll('[id*=operater]');
 const equal = document.querySelector('.equal');
-const allclear = document.querySelector('.all-clear');
-const deletelast = document.querySelector('.delete');
+const allClear = document.querySelector('.all-clear');
+const deleteLast = document.querySelector('.delete');
 
 class Calculator {
     constructor(previousDisplay, resultDisplay) {
@@ -17,4 +17,18 @@ class Calculator {
         this.resultDisplay = '';
         this.operation = undefined;
     }
-};
+
+    updateDisplay() {
+        this.previousDisplay = this.previousDisplay;
+        this.resultDisplay = this.resultDisplay;
+    }
+}
+
+const calculator = new Calculator(
+    previousDisplay,
+    resultDisplay
+);
+
+allClear.addEventListener('click', () => {
+    this.clear();
+})
